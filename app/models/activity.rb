198d8bbe7b91a46cb43539464c3d9110b706c734
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-01 13:51:43 -0400

    added validation and categories_attributes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,15 @@ class Activity < ApplicationRecord
     accepts_nested_attributes_for :categories
     validates :name, presence: true
     validates :name, uniqueness: true
+
+    def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      if category_attributes[:title].present?
+      category = Category.find_or_create_by(title: category_attributes[:title])
+        if !self.categories.include?(category)
+          self.activities_categories.build(:category => category)
+        end
+      end
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     has_and_belongs_to_many :activities
-    accepts_nested_attributes_for :activities
-    validates :name, presence: true
+    
+    validates :title, presence: true
+    validates :title, uniqueness: true
 end
