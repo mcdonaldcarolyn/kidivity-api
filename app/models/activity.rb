0ab41ffd3104b0a5049dfd7936f6cf88d4e3089e
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-01 12:50:33 -0400

    add active record validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ApplicationRecord
     has_and_belongs_to_many :categories
+    accepts_nested_attributes_for :categories
+    validates :name, presence: true
+    validates :name, uniqueness: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_and_belongs_to_many :activities
+    accepts_nested_attributes_for :activities
+    validates :name, presence: true
 end
