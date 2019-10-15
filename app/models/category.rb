a995206ea047eb066c21ea049f8b7b67d66ebea5
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-15 06:33:43 -0400

    clean up category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,5 @@
 class Category < ApplicationRecord
     has_many :activities
-    # for scale
-    # has_and_belongs_to_many :activities
     
     validates :title, presence: true
     validates :title, uniqueness: true
