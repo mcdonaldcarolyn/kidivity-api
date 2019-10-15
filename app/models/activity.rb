619bfa993f88622a0221a64e2fd86c6e3ad67043
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-15 06:33:07 -0400

    clean up activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,19 +2,5 @@ class Activity < ApplicationRecord
   belongs_to :category
   validates :name, presence: true
   validates :name, uniqueness: true 
-  # for scale
-    # has_and_belongs_to_many :categories
-    #  accepts_nested_attributes_for :categories
-    
-    # for scale
-#     def categories_attributes=(categories_hashes)
-#       categories_hashes.each_with_index do | category_attributes, i|
-
-#         category = Category.find_or_create_by(title: category_attributes["title"])
-#         if !self.categories.include?(category)
-#           self.categories << category
-           
-#       end
-#     end
-#   end
+  
 end
