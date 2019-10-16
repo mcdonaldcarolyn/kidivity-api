6df5a8a6d3588255056c2d3e032aa8a47a432fe2
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-10-16 14:05:40 -0400

    fix seed file to have 2 activities per category

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ Category.create(title: 'Tweens and Teens')
 
 Activity.create(name: 'pool', address: 'saugus, ma', description: 'great pool', category_id: 1)
 Activity.create(name: 'park ', address: 'melrose st', description: 'great park', category_id: 1)
-Activity.create(name: 'park ', address: 'melrose st', description: 'great park', category_id: 2)
+
 Activity.create(name: 'zoo', address: 'stoneham, ma', description: 'great zoo', category_id: 2)
-Activity.create(name: 'lego museum', address: 'somerville, ma', description: 'great spot for legos', category_id: 3)
+Activity.create(name: 'lego museum', address: 'somerville, ma', description: 'great spot for legos', category_id: 2)
 Activity.create(name: 'boda borg', address: 'malden, ma', description: 'escape room', category_id: 3)
 Activity.create(name: 'movies', address: 'revere, ma', description: 'see a movie', category_id: 3)
 
